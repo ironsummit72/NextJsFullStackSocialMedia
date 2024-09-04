3c0db02467dc2b53011da5c5636a3ffb6f14fd2c
ironsummit72 <ironsumit72@gmail.com>
2024-09-04 19:16:16 +0530

    feat: add zodValidations.ts for backend validation of register and login requests

---
+++ backend/src/validation/zodValidations.ts
@@ -0,0 +1,27 @@
+import {z} from 'zod'
+export const UserRegisterSchema = z
+	.object({
+		username: z
+			.string()
+			.min(3, 'Username must be at least 3 characters')
+			.max(20, 'username cannot be more than 20 characters')
+			.trim(),
+		email: z.string().email('Invalid email format. please enter a valid email address').trim(),
+		firstname: z.string().min(3, 'Username must be at least 3 characters').trim(),
+		lastname: z.string().min(3, 'Username must be at least 3 characters').trim(),
+		password: z.string().min(5, 'Password must be at least 5 characters').trim(),
+		cpassword: z.string(),
+	})
+	.refine((data) => data.password === data.cpassword, {
+		message: 'confirm password not match with password',
+		path: ['cpassword'],
+	})
+
+export const userLoginWithUsername = z.object({
+	username: z.string().min(3, 'username must be at least 5 characters').trim(),
+	password: z.string().min(5, 'password must be at least 5 characters'),
+})
+export const userLoginWithEmail = z.object({
+	email: z.string().email('Please Enter Your valid email address').trim(),
+	password: z.string().min(5, 'password must be at least 5 characters'),
+})
