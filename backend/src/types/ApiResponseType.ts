4c03d72757398f8983f0af489fd72605f96e14f0
ironsummit72 <ironsumit72@gmail.com>
2024-09-04 19:10:43 +0530

    feat: add custom types for ApiResponse to standardize API responses

---
+++ backend/src/types/ApiResponseType.ts
@@ -0,0 +1,12 @@
+import { Request,Response } from "express"
+export interface ApiResponseType {
+	success: boolean
+	statusCode: number
+	statusMessage: string
+	data: any | null
+	message: string | any
+	redirect: string | null
+}
+export interface CustomRequest extends Request {
+	user?: object
+}
