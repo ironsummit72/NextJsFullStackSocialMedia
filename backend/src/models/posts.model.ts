230f05ea24daf31b1b3e896b51ee4e0f68684b7b
ironsummit72 <ironsumit72@gmail.com>
2024-09-04 19:04:14 +0530

    feat: add post.model.ts with Mongoose schema for PostModel

---
+++ backend/src/models/posts.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+import { PostModel } from "../types/types";
+const PostSchema = new Schema<PostModel>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  content: [],
+});
+const postModel = mongoose.model("PostModel", PostSchema);
+export default postModel;
