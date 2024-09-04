5fb756700090c4ca54876f6489ef340f1d9e80b4
ironsummit72 <ironsumit72@gmail.com>
2024-09-04 18:56:40 +0530

    feat: format index.ts, add authRouter and postRouter, and implement user middleware for global access to user data in requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,33 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import authRouter from './routes/auth.route'
-const PORT = 5002;
-const app = express();
+import postRouter from './routes/post.route'
+
+import ConnectDB from './utils/connectDb'
+import cookieParser from 'cookie-parser'
+import getCurrentUser from './middleware/CurrentUser.middleware'
+import isUserAuthenticated from './middleware/CheckAuth.middleware'
+const PORT = 5002
+const app = express()
 app.use(
-  cors({
-    origin: ["http://localhost:3000"],credentials:true
-  })
-);
-app.use(morgan("tiny"));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+	cors({
+		origin: ['http://localhost:3000'],
+		credentials: true,
+	}),
+)
+app.use(cookieParser())
+app.use(morgan('tiny'))
+ConnectDB()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-//routes 
-app.use("/auth",authRouter)
-
-
+//routes
+app.use('/auth', authRouter)
+app.use(getCurrentUser)
+app.use(isUserAuthenticated)
+app.use('/post',postRouter)
 
 app.listen(PORT, () => {
-  console.log("listening on port " + PORT);
-});
+	console.log('listening on port ' + PORT)
+})
