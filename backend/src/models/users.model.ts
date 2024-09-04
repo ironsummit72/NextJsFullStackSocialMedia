86eb0ee9ac3d98655dec25c2885ecdd530178aab
ironsummit72 <ironsumit72@gmail.com>
2024-09-04 19:04:49 +0530

    feat: add user.model.ts with Mongoose schema for UserModel

---
+++ backend/src/models/users.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import { UserModel } from "../types/types";
+import { hashPassword } from "../utils/genHashPassword";
+const userSchema = new Schema<UserModel>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  posts: [{ type: Schema.Types.ObjectId, ref: "post" }],
+});
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    } else {
+      const hashedPassword = await hashPassword(this.password);
+      this.password = hashedPassword;
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
