c91fce11f2dfeee153ae81b6c7042be0c1cc062e
ironsummit72 <ironsumit72@gmail.com>
2025-01-19 02:11:02 +0530

    feat(post): Added multer upload middleware in create post route

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express'
 import {createPost, editPost, deletePost, getAllPosts, getPostById} from '../controller/post.controller'
+import upload from '../middleware/multer.middleware'
 const router = Router()
 
-router.post('/create', createPost)
+router.post('/create',upload.array('posts'), createPost)
 router.get('/:id', getPostById)
 router.get('/', getAllPosts)
 router.patch('/:id', editPost)
