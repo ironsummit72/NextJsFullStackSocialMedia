0947fa20df17c07b464436b6999716927d784de2
ironsummit72 <ironsumit72@gmail.com>
2025-01-20 11:36:19 +0530

    feat(models): Added db fields in post.model.ts like comments, hashtags, likes, mentions

--- backend/src/models/posts.model.ts
+++ backend/src/models/posts.model.ts
@@ -1,15 +1,44 @@
-import mongoose, { Schema } from "mongoose";
-import { PostModel } from "../types/DatabaseModelTypes";
+import mongoose, {Schema} from 'mongoose'
+import {PostModel} from '../types/DatabaseModelTypes'
 const PostSchema = new Schema<PostModel>({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  caption: {
-    type: String,
-    required: true,
-  },
-  content: [],
-});
-const postModel = mongoose.model("PostModel", PostSchema);
-export default postModel;
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+	},
+	caption: {
+		type: String,
+		required: true,
+	},
+	content: [],
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+      default:[]
+		},
+	],
+	hashtags: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Hashtag',
+      default:[]
+		},
+	],
+	likes: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+      default:[]
+		},
+	],
+	mentions: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+      default:[]
+		},
+	],
+
+})
+const postModel = mongoose.model('PostModel', PostSchema)
+export default postModel
