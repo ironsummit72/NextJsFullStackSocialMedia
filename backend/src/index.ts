f985e2ba2bb5fc79e235f2830a48951aa7140a5f
ironsummit72 <ironsumit72@gmail.com>
2025-02-19 14:41:26 +0530

    Refactor route files: Rename auth.route.ts and post.route.ts
    - Rename `auth.route.ts` to `auth.routes.ts`
    - Rename `post.route.ts` to `post.routes.ts`
    - Update all

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-import authRouter from './routes/auth.route'
-import postRouter from './routes/post.route'
-
+import authRouter from './routes/auth.routes'
+import postRouter from './routes/post.routes'
+import jwt from 'jsonwebtoken'
 import ConnectDB from './utils/connectDb.util'
 import cookieParser from 'cookie-parser'
 import getCurrentUser from './middleware/CurrentUser.middleware'
 import isUserAuthenticated from './middleware/CheckAuth.middleware'
+import { ApiResponseType } from './types/ApiResponseType'
 const PORT = 5002
 const app = express()
 app.use(
@@ -24,6 +25,26 @@ app.use(express.urlencoded({extended: true}))
 
 //routes
 app.use('/auth', authRouter)
+app.get('/currentuser',(req,res)=>{
+	const token = req.cookies.sessionId
+	if (token) {
+		//@ts-ignore
+		console.log(token,"token");
+		
+		jwt.verify(token, process.env.JWT_SECRET!, (err, decodedToken) => {
+			if(err)
+			{
+				const response:ApiResponseType={data:null,success:false,statusCode:401,statusMessage:'unauthorize access',message:'unauthorize access',redirect:'/login'}
+		        res.json(response)
+			}
+			const response:ApiResponseType={data:decodedToken,success:true,statusCode:200,statusMessage:'success',message:'current user',redirect:null}
+			res.json(response)
+		})
+	} else {
+		const response:ApiResponseType={data:null,success:false,statusCode:401,statusMessage:'unauthorize access',message:'unauthorize access',redirect:'/login'}
+		res.json(response)
+	}
+})
 app.use(getCurrentUser)
 app.use(isUserAuthenticated)
 app.use('/post',postRouter)
