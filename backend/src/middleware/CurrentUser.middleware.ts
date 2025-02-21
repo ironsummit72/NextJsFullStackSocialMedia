4abf6f5e90cab8ec95de9d33c8ea4f3a9ced4a18
ironsummit72 <ironsumit72@gmail.com>
2025-02-21 15:13:10 +0530

    Remove Custom Request Type in TypeScript
    - Refactor code to use standard Express Request type
    - Improve maintainability and reduce unnecessary type complexity
    - Ensure all affected files are updated accordingly

--- backend/src/middleware/CurrentUser.middleware.ts
+++ backend/src/middleware/CurrentUser.middleware.ts
@@ -1,11 +1,11 @@
 import {Request, Response, NextFunction} from 'express'
 import jswonwebtoken from 'jsonwebtoken'
 import envs from '../conf/env'
-import {CustomRequest} from '../types/ApiResponseType'
 
-export default function getCurrentUser(req: CustomRequest, _: Response, next: NextFunction) {
+
+export default function getCurrentUser(req: Request, _: Response, next: NextFunction) {
 	const token = req.cookies?.sessionId
-	const token2 = req.cookies
+	
 
 	const secret = envs.JWT_SECRET || ''
 	if (token) {
