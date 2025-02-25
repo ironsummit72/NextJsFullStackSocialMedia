b597b20f5ce96ec1407081e75b4075bb5e4e83b3
ironsummit72 <ironsumit72@gmail.com>
2025-02-25 14:15:55 +0530

    Update Multer middleware for media uploads
    - Store videos in `post/videos` directory
    - Store photos in `post/images` directory (fixed incorrect path)
    - Ensure proper file handling and directory creation

--- backend/src/middleware/multer.middleware.ts
+++ backend/src/middleware/multer.middleware.ts
@@ -3,15 +3,15 @@ import fs from 'node:fs'
 import path from 'node:path'
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		if (fs.existsSync(`./uploads/${file?.fieldname}`)) {
-			cb(null, `./uploads/${file?.fieldname}`)
+		if (fs.existsSync(`./uploads/${file?.fieldname}/${file.mimetype.split('/')[0]}`)) {
+			cb(null, `./uploads/${file?.fieldname}/${file.mimetype.split('/')[0]}`)
 		} else {
-			fs.mkdir(`./uploads/${file?.fieldname}`, (err) => {
+			fs.mkdir(`./uploads/${file?.fieldname}/${file.mimetype.split('/')[0]}`,{recursive:true}, (err) => {
 				if (err) {
 					console.error(err)
 				}
 				console.log('directory created successfully')
-				cb(null, `./uploads/${file?.fieldname}`)
+				cb(null, `./uploads/${file?.fieldname}/${file.mimetype.split('/')[0]}`)
 			})
 		}
 	},
