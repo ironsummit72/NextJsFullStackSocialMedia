70f4d94c1c298111ba7fd88dc215cea9f61aa6ce
ironsummit72 <ironsumit72@gmail.com>
2025-03-28 13:27:33 +0530

    Add new types in index.d.ts
    - Define `UserData` type for structured user information
    - Add `ContentType` type for handling different media formats
    - Introduce `PostData` type for consistent post data representation
    - Improve type safety and maintainability across the application

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,33 @@ export interface USER  {
     fullName: string,
     id: string
   }
+  interface UserData {
+    _id:string,
+    username:string,
+    email:string,
+    firstName:string,
+    lastname:string,
+    displayPicturePath:string,
+
+  }
+  type contentType={
+    fieldname: string,
+    originalname:string
+    encoding: string,
+    mimetype: string,
+    destination: string,
+    filename: string,
+    path: string,
+    size: number
+  }
+  export interface PostData{
+    _id:string,
+    user:UserData,
+    caption:string,
+    content:Array<contentType>
+    //TODO:👇 define there types later 
+    comments:[]
+    hastags:[]
+    likes:[]
+    mentions:[]
+  }
