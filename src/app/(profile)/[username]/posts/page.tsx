6a35893fe2aae4bde8900567defc7ef942538568
ironsummit72 <ironsumit72@gmail.com>
2025-04-08 09:47:48 +0530

    Add posts and tagged pages to Profile route group
    - Implement `[username]/posts/page.tsx` to show user's posts
    - Implement `[username]/tagged/page.tsx` to show tagged posts
    - Enhance user profiles with organized content sections

---
+++ src/app/(profile)/[username]/posts/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+import Video from '@/components/custom/Video';
+import { clientapi } from '@/lib/api';
+import { PostData } from '@/types';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
+
+type Props = {
+    params: Promise<{ username: string }>
+}
+
+function PostsHome({ params }: Props) {
+    const { username } = React.use(params);
+    const [data, setData] = useState<PostData[] | null>(null);
+    async function fetchData(pageNumber: number = 1) {
+        const response = await clientapi.get(`/post/user/${username}`)
+        setData(response.data.data.posts)
+        //   setEndPage(response.data.data.endPage)
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+    console.log("post data", data);
+    return (
+        <div className='grid grid-cols-3 gap-3 mt-5 ' >
+            {data && <>
+                {data?.map((postData: PostData) => {
+                    if (postData.content[0].mimetype.split('/')[0] === 'video') {
+                        return <Link key={postData._id} href={`/post/${postData._id}`}><Video filename={`${postData.content[0].filename}`}></Video></Link>
+                    } else {
+                        return <Link key={postData._id} href={`/post/${postData._id}`}><img key={postData._id} src={`http://localhost:5002/content/stream/${postData.content[0].mimetype.split('/')[0]}/${postData.content[0].filename}`} width={400} height={400} alt="" /></Link>
+                    }
+                })}
+            </>}
+        </div>
+    )
+}
+
+export default PostsHome
