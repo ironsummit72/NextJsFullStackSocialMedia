f2a19fc4433457d1f27576a2d196d15dfdd66f28
ironsummit72 <ironsumit72@gmail.com>
2025-04-08 12:32:38 +0530

    Enable infinite scrolling in Profile and Posts pages
    - Update `[username]/page.tsx` and `[username]/posts/page.tsx`
    - Integrate intersection observer for seamless post loading
    - Improve UX with smooth, paginated content fetching

--- src/app/(profile)/[username]/posts/page.tsx
+++ src/app/(profile)/[username]/posts/page.tsx
@@ -1,26 +1,31 @@
 'use client'
+import Spinner from '@/components/custom/Spinner';
 import Video from '@/components/custom/Video';
 import { clientapi } from '@/lib/api';
 import { PostData } from '@/types';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
+import { InView } from 'react-intersection-observer';
+
 
 type Props = {
     params: Promise<{ username: string }>
 }
-
 function PostsHome({ params }: Props) {
     const { username } = React.use(params);
     const [data, setData] = useState<PostData[] | null>(null);
+    const [page, setPage] = useState<number>(2);
+    const [endPage, setEndPage] = useState<number>(0)
     async function fetchData(pageNumber: number = 1) {
-        const response = await clientapi.get(`/post/user/${username}`)
-        setData(response.data.data.posts)
-        //   setEndPage(response.data.data.endPage)
+        const response = await clientapi.get(`/post/user/${username}?page=${pageNumber}&limit=10`)
+        setData(response.data.data.postData)
+        console.log('dp', response.data.data)
+        setEndPage(response.data.data.endPage)
     }
     useEffect(() => {
         fetchData()
     }, [])
-    console.log("post data", data);
+
     return (
         <div className='grid grid-cols-3 gap-3 mt-5 ' >
             {data && <>
@@ -32,8 +37,26 @@ function PostsHome({ params }: Props) {
                     }
                 })}
             </>}
+            <InView as={'div'} onChange={async () => {
+                if (endPage >= page) {
+                    const response = await clientapi.get(`/post/user/${username}?&page=${page}&limit=10`)
+                    setData((data) => [...data!, ...response.data.data.postData])
+                    setPage((page) => page + 1)
+                }
+            }}>
+            </InView>
+            <div className='w-full flex items-center  justify-center gap-3'>
+                {page >= endPage ? <h1>{`that's all folks ${endPage}`}</h1> : <>
+                    <Spinner />
+                    <h1>loading...</h1>
+                </>
+                }
+            </div>
         </div>
     )
 }
 
 export default PostsHome
+
+
+
