e4e57069c6e09c82795dee648cb840e18b834ac6
ironsummit72 <ironsumit72@gmail.com>
2025-04-12 13:18:35 +0530

    feat(profile): add following page to show user's following list

--- src/app/(profile)/(.)[username]/following/page.tsx
+++ src/app/(profile)/(.)[username]/following/page.tsx
@@ -1,21 +1,105 @@
 'use client'
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { DialogTitle } from '@radix-ui/react-dialog'
-import React from 'react'
-import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { clientapi } from '@/lib/api'
+import { USER, UserData } from '@/types'
+import HoverCardProfile from '@/components/custom/HoverCard/HoverCardProfile'
+import DisplayPicture from '@/components/custom/DisplayPicture'
+import { Button } from '@/components/ui/button'
+import { useToast } from '@/hooks/use-toast'
+import { getCurrentUserClient } from '@/lib/getCurrentUserClient'
+import Link from 'next/link'
+
+interface customUserData extends UserData {
+  followers: UserData[]
+}
+interface Data {
+  dbResponse: customUserData,
+  endPage: number
+}
+
 function Following() {
+  const pathname = usePathname().split('/')[1];
+  const [data, setData] = useState<Data>()
   const router = useRouter()
   const onHandleClose = () => {
     router.back()
   }
+  useEffect(() => {
+    clientapi.get(`/profile/following/${pathname}?page=1&limit=10`).then((res) => {
+      console.log(res.data);
+      setData(res.data.data)
+    }).catch((error) => {
+      console.error(error);
+
+    })
+  }, [])
   return (
     <Dialog open={true} defaultOpen={true} onOpenChange={onHandleClose} >
-      <DialogTitle>Hello</DialogTitle>
       <DialogContent>
-        Intercepted Post following
+        <DialogTitle className='text-center'><span className='text-lg font-bold'>Following</span></DialogTitle>
+        <div className='flex flex-col w-full items-center'>
+          {data?.dbResponse.following.map((data) => {
+            return <HoverCardProfile key={data._id} username={data.username}>
+             <div className='w-[90%]'>
+             <Content username={data.username} data={data} />
+             </div>
+            </HoverCardProfile>
+          })}
+        </div>
       </DialogContent>
     </Dialog>
   )
 }
 
 export default Following
+
+type ContentProps = {
+  username: string,
+  data: UserData
+}
+
+function Content({ username, data }: ContentProps) {
+  const [isFollowing, setIsFollowing] = useState<boolean>();
+  const [user, setUser] = useState<USER | null>(null);
+  useEffect(() => {
+    getCurrentUserClient().then((res) => {
+      setUser(res);
+    }).catch((error) => {
+      setUser(null);
+      console.error(error);
+    })
+  }, [])
+  useEffect(() => {
+    if (!data) return;
+    clientapi.get(`/user/isfollowing/${data?._id}`).then((res) => {
+      console.log(res.data.data, 'isFollowing');
+      setIsFollowing(res.data.data);
+    }).catch((err) => {
+      console.error(err);
+    })
+  }, [data])
+  const { toast } = useToast()
+  const onHandleFollow = (userId: string) => {
+    clientapi.post(`/user/follow/${userId}`).then((res) => {
+      toast({
+        title: "follow",
+        description: res.data?.message
+      })
+    })
+  }
+  return <div className='flex items-center justify-center cursor-pointer  min-w-[90%]'>
+    <div className='flex gap-4 items-center p-2  w-full m-3 rounded-md '>
+      <DisplayPicture username={username} width={40} height={40} />
+      <div className='max-w-[90%] w-[90%]'>
+        <h1 className='font-bold' title='username'>{username}</h1>
+        <h3 title='info' className='text-gray-500'>{data.firstName} {data.lastName}</h3>
+      </div>
+      <div>
+        {username === user?.username ? <Button asChild ><Link href={'/accounts/edit'}>Edit Profile</Link></Button> : <Button variant={isFollowing ? "outline" : 'default'} onClick={() => onHandleFollow(data._id)}>{isFollowing ? "Following" : "Follow"}</Button>}
+      </div>
+    </div>
+  </div>
+}
