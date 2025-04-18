1a748eb725c8af0ce7517c49a462106f43053ba7
ironsummit72 <ironsumit72@gmail.com>
2025-04-19 01:26:52 +0530

    feat(video): add className prop to Video component for custom styling

--- src/components/custom/Video.tsx
+++ src/components/custom/Video.tsx
@@ -1,23 +1,27 @@
 'use client'
-import {  ClapperboardIcon } from "lucide-react";
+import { ClapperboardIcon } from "lucide-react";
 import { useRef } from "react"
-type Props={
-    filename:string
+import { twMerge } from "tailwind-merge";
+type Props = {
+  filename: string
+  width?: number | string
+  height?: number | string
+  className?: string
 }
-function Video({filename}:Props) {
-  const videoRef=useRef<HTMLVideoElement>(null);
-  const handleMouseOver=()=>{
+function Video({ filename, height, width, className }: Props) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const handleMouseOver = () => {
     videoRef.current?.play()
   }
-  const handleMouseLeave=()=>{
+  const handleMouseLeave = () => {
     videoRef.current?.pause()
   }
   return (
-  <div className="relative">
-    <ClapperboardIcon className="absolute text-white m-4" size={30}  />
-     <video className="cursor-pointer " ref={videoRef} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} src={`http://localhost:5002/content/stream/video/${filename}`}>
-     </video>
-  </div>
+    <div className="relative">
+      <ClapperboardIcon className="absolute text-white m-4" size={30} />
+      <video width={width} height={height} className={twMerge("cursor-pointer ", className)} ref={videoRef} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} src={`http://localhost:5002/content/stream/video/${filename}`}>
+      </video>
+    </div>
 
   )
 }
