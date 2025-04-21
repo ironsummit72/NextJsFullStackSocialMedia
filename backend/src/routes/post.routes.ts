821759f5f63f26f87c70599488afbd9c3a6b8b1a
ironsummit72 <ironsumit72@gmail.com>
2025-04-21 12:30:32 +0530

    feat(post): add isLiked and isSaved routes in post.routes.ts

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -1,15 +1,16 @@
-import {Router} from 'express'
-import {createPost, editPost, deletePost, getAllPostsByUsername, getPostById,recommendedPosts, likePost, savePost} from '../controller/post.controller'
+import { Router } from 'express'
+import { createPost, editPost, deletePost, getAllPostsByUsername, getPostById, recommendedPosts, likePost, savePost, isSaved, isLiked } from '../controller/post.controller'
 import upload from '../middleware/multer.middleware'
 const router = Router()
 
 router.get('/:id', getPostById)
 router.get('/user/:username', getAllPostsByUsername)
-router.get('/r/recommended',recommendedPosts)
-//TODO: explore post
-router.post('/create',upload.array('posts'), createPost)
+router.get('/r/recommended', recommendedPosts)
+router.get('/isliked/:postId', isLiked)
+router.get('/issaved/:postId', isSaved)
+router.post('/create', upload.array('posts'), createPost)
 router.patch('/:id', editPost)
-router.patch('/like/:id',likePost)
-router.patch('/save/:id',savePost)
+router.patch('/like/:postId', likePost)
+router.patch('/save/:postId', savePost)
 router.delete('/:id', deletePost)
 export default router
