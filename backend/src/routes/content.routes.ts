e065ead053325ce12b9c6edd210375e8899077ce
ironsummit72 <ironsumit72@gmail.com>
2025-08-21 15:49:33 +0530

    feat(content): add route for streaming stories

--- backend/src/routes/content.routes.ts
+++ backend/src/routes/content.routes.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express'
-import { streamContent } from '../controller/content.controller';
+import { streamContent, streamContentForStories } from '../controller/content.controller';
 
 const router=Router();
 router.get('/stream/:dir/:filename',streamContent)
+router.get('/streamstories/:dir/:filename',streamContentForStories)
 export default router
